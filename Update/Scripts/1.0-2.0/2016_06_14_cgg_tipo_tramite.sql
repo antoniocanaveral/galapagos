275d21d53eccd534082d2f6b599a8c792d16bcb6
mabelortiz <mabel.ortiz@ac-techsolutions.com>
2016-06-14 17:52:18 -0500

    scripts tipo tramite

---
+++ Update/Scripts/1.0-2.0/2016_06_14_cgg_tipo_tramite.sql
@@ -0,0 +1,16 @@
+﻿-- Table: sii.cgg_tipo_tramite
+
+-- DROP TABLE sii.cgg_tipo_tramite;
+
+CREATE TABLE sii.cgg_tipo_tramite
+(
+  crtt_codigo character varying(20) NOT NULL,
+  crtt_nombre character varying(150),
+  crtt_estado boolean,
+  CONSTRAINT pk_cgg_prueba PRIMARY KEY (crtt_codigo)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE sii.cgg_tipo_tramite
+  OWNER TO postgres;
