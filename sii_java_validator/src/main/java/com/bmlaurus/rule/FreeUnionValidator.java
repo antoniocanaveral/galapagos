0111e0a02e50cd923bc12f31b331260015b23c09
ximenasilva <ximena.silva@ac-techsolutions.com>
2016-06-22 10:31:49 -0500

    reglas

--- sii_java_validator/src/main/java/com/bmlaurus/rule/FreeUnionValidator.java
+++ sii_java_validator/src/main/java/com/bmlaurus/rule/FreeUnionValidator.java
@@ -1,6 +1,7 @@
 package com.bmlaurus.rule;
 
 import com.besixplus.sii.objects.Cgg_regla_validacion_metadatos;
+import com.bmlaurus.rule.RuleClass;
 import org.json.JSONObject;
 
 /**
@@ -10,6 +11,12 @@ public class FreeUnionValidator implements RuleClass {
 
     @Override
     public String executeRule(Cgg_regla_validacion_metadatos metadata, JSONObject regla) {
-        return "true";
+        boolean unionDeHecho = true;//aqui se debe llamar al servicio web para que devuelva la union de hecho
+    	
+    	if(unionDeHecho){
+    		 return "true";
+    	}else{
+    		 return "false,"+regla.getString("CRVAL_SUGERENCIA");
+    	}
     }
 }
