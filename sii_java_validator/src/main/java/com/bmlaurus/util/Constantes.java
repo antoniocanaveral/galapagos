9eb3ddc75b0727318a45dab4f4e43b92d73511b6
ximenasilva <ximena.silva@ac-techsolutions.com>
2016-07-07 16:18:10 -0500

    Se colocó el mensaje en las reglas java para controlar si viene algú error del webservice

--- sii_java_validator/src/main/java/com/bmlaurus/util/Constantes.java
+++ sii_java_validator/src/main/java/com/bmlaurus/util/Constantes.java
@@ -8,5 +8,6 @@ public class Constantes {
     public final static String MENSAJE_DATOS_NULOS = "El servicio del Registro Civíl no devolvió datos.";
     public final static String MENSAJE_SOLICITUD_TEMPORAL = "Necesita solictar la residencia temporal por mínimo 10 años";
     public final static String SOLICITUD_41_1B_CONVIVIENYE_RESIDENTE_PERM = "CRTST353";
+    public final static String MENSAJE_SOLICITUD_TEMPORAL_FECHA = "Fecha de recepción de la solicitud de residencia esta vacia.";
     
 }
