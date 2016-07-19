35507a46088916441c402b3ea57a22c35a7d7488
mabelortiz <mabel.ortiz@ac-techsolutions.com>
2016-07-19 15:17:44 -0500

    cambio regla de validacion Beneficiario identificado en el Registro civil (para que pase en el caso de pasaporte y en el caso de registro interno como externo)

--- sii_java_validator/src/main/java/com/bmlaurus/rule/RegisteredIdentificationBeneficiary.java
+++ sii_java_validator/src/main/java/com/bmlaurus/rule/RegisteredIdentificationBeneficiary.java
@@ -2,32 +2,57 @@ package com.bmlaurus.rule;
 
 
 
+import com.besixplus.sii.db.ManagerConnection;
 import com.besixplus.sii.objects.Cgg_regla_validacion_metadatos;
+import com.besixplus.sii.objects.Cgg_res_persona;
 import com.bmlaurus.ws.dinardap.RegistroCivil;
 import org.json.JSONObject;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class RegisteredIdentificationBeneficiary implements RuleClass {
 
     @Override
     public String executeRule(Cgg_regla_validacion_metadatos metadata,
 			JSONObject regla, RuleData ruleData) {
-    	
-    	
-    	   String error = regla.getString("CRVAL_SUGERENCIA");
-         	
-    	   RegistroCivil registroCivil = new RegistroCivil(ruleData.getCRPER_NUM_DOC_IDENTIFIC());//cedula del beneficiario
-           if(registroCivil.callServiceAsObject().equals(RegistroCivil.CALL_ERROR)){
-               if(registroCivil.getResultMap()!=null)
-                   error = (String) registroCivil.getResultMap().get(RegistroCivil.KEY_MENSAJE);
-               else
-                   return "true,"+RegistroCivil.SERVICE_ERROR;
-           }else{
-        	   if(registroCivil.getCedula()!=null && !registroCivil.getCedula().trim().isEmpty()){
-         	    	return "true";
-               }   
-           }
+
+        String error = regla.getString("CRVAL_SUGERENCIA");
+        Cgg_res_persona beneficiario = new Cgg_res_persona();
+
+        if(ruleData.getCRDID_CODIGO().equals("")) {
+            try {
+                Connection con = null;
+                con = ManagerConnection.getConnection();
+                con.setAutoCommit(false);
+
+                beneficiario.setCRPER_CODIGO(ruleData.getCGGCRPER_CODIGO());
+                beneficiario = new com.besixplus.sii.db.Cgg_res_persona(beneficiario).select(con);
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+            if (beneficiario.getCRDID_CODIGO().equals("1") || ruleData.getCRDID_CODIGO().equals("1") ){
+
+                RegistroCivil registroCivil = new RegistroCivil(ruleData.getCRPER_NUM_DOC_IDENTIFIC());//cedula del beneficiario
+                if (registroCivil.callServiceAsObject().equals(RegistroCivil.CALL_ERROR)) {
+                    if (registroCivil.getResultMap() != null)
+                        error = (String) registroCivil.getResultMap().get(RegistroCivil.KEY_MENSAJE);
+                    else
+                        return "true," + RegistroCivil.SERVICE_ERROR;
+                } else {
+                    if (registroCivil.getCedula() != null && !registroCivil.getCedula().trim().isEmpty()) {
+                        return "true";
+                    }
+                }
+            }else {
+                return "true";
+            }
           
            return "false,"+error;
         
     }
+
 }
